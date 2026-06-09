1951fd9662bae90c6396224e788705a22db7b6cc
karthikjayavaram <karthik.jayavaram@gmail.com>
2026-06-09 13:26:26 +0530

    Fix deployment dependencies

--- langraph_backend.py
+++ langraph_backend.py
@@ -4,9 +4,10 @@ from langchain_core.messages import BaseMessage
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
-from dotenv import load_dotenv
+import os
 
-load_dotenv()
+token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
 
 
 llm = HuggingFaceEndpoint(  
